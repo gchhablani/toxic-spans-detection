b1e2cc8c2a20a1edfa0eaa60d3d1c5eaa0fe28c2
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2021-02-19 06:44:31 +0530

    Minor fix

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,3 +1,4 @@
 from src.datasets.toxic_spans_tokens import *
 from src.datasets.toxic_spans_spans import *
 from src.datasets.toxic_spans_tokens_spans import *
+from src.datasets.toxic_spans_multi_spans import *
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,5 @@
 from src.models.auto_models import *
 from src.models.bert_token_spans import *
 from src.models.roberta_token_spans import *
+from src.models.bert_multi_spans import *
+from src.models.roberta_multi_spans import *
